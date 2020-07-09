b649200ffe541fb710e1c6efa2ba54affbdbccec
Conni Simmons <connisimmons@gmail.com>
2020-07-08 19:37:48 -0600

    updated port

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+PORT = 3729
 const app = express();
 
 
@@ -10,9 +10,9 @@ app.get('/', (req, res, next) => {
 
 // just adding a new comment to push to github.
 
-app.listen(3729, (err) => {
+app.listen(PORT, (err) => {
     if (err) {
         console.log('Error starting server: ', err);
     }
-    console.log('Server listening on port 3729.');
+    console.log(`Server listening on port ${3729}.`);
 })
